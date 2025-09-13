9a13eebefa74d43928b54186d264c248f67f2a33
Mudit-Tarway <official.mudittarway@gmail.com>
2025-09-13 18:22:29 +0530

    Added the code for Binary Search

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch{
+    public static void main(String args[]){
+        int arr[] = {1 , 2 , 3, 4, 5, 6, 7, 8 , 9};
+        int target = 8;
+        int index = binarySearch(arr , target);
+        if(index == -1){
+            System.out.println("Element not found");
+        }else{
+            System.out.println("Element "+target+" found at index "+index);
+        }
+    }
+    public static int binarySearch(int arr[] , int target){
+        int start = 0;
+        int end = arr.length -1 ;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
