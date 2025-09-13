0416358625e1026b7f2dcc59b21223060b2d2da2
Mudit-Tarway <official.mudittarway@gmail.com>
2025-09-13 18:45:04 +0530

    Code for merge sort

---
+++ Sorting/Merge.java
@@ -0,0 +1,46 @@
+public class Merge {
+    public static void mergeSort(int[] arr , int si , int ei) {
+        if(si >= ei) {
+            return;
+        }
+        int mid = (si + ei) / 2;
+        mergeSort(arr, si, mid);    
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
+    }
+    private static void merge(int[] arr, int si , int mid , int ei) {
+        int[] temp = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1; 
+        int k = 0; 
+
+        while(i <= mid && j <= ei) {
+            if(arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while(j <= ei) {
+            temp[k++] = arr[j++];
+        }
+        for(k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
